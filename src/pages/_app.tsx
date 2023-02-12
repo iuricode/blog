d1111be85cc4602b15468286fc0ff1c9378312ed
iuricode <iuricold99@gmail.com>
2023-02-11 21:23:31 -0300

    adicionando o analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from "next/app";
 import React from "react";
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <React.Fragment>
       <Component {...pageProps} />
+      <Analytics />
     </React.Fragment>
   );
 };
