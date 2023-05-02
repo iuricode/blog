85029c40a606977019f055b0c5f61d55569d3da5
Iuri Silva <iuricold99@gmail.com>
2023-05-02 14:11:07 -0300

    Update setup.tsx

--- src/pages/setup.tsx
+++ src/pages/setup.tsx
@@ -27,7 +27,7 @@ const Setup = () => {
                   <a
                     href="https://www.kabum.com.br/produto/113859/headset-gamer-husky-gaming-polar-preto-p2-drivers-50mm-rainbow-hgmd002"
                     target="_blank"
-                     rel="noopener"
+                     rel="noreferrer"
                   >
                     Headset Gamer - Husky Gaming Polar
                   </a>
@@ -36,7 +36,7 @@ const Setup = () => {
                   <a
                     href="https://www.pichau.com.br/cadeira-gamer-pichau-tippler-vermelha-by-8105-vermelho"
                     target="_blank"
-                     rel="noopener"
+                     rel="noreferrer"
                   >
                     Cadeira Gamer - Pichau Tippler Vermelha
                   </a>
@@ -45,7 +45,7 @@ const Setup = () => {
                   <a
                     href="https://www.pichau.com.br/gabinete-gamer-pichau-magpie-iii-led-azul-lateral-frontal-vidro-pgma-03-blue?gclid=Cj0KCQjwqPGUBhDwARIsANNwjV4iSD_5SpqtY_jg0dJ2QR4azO44IIChfjDq-6ysQUDJS69ryKQBss4aAmdPEALw_wcB"
                     target="_blank"
-                     rel="noopener"
+                     rel="noreferrer"
                   >
                     Gabinete Gamer - Pichau Magpie III
                   </a>
@@ -54,7 +54,7 @@ const Setup = () => {
                   <a
                     href="https://www.kabum.com.br/produto/93157/teclado-mecanico-gamer-redragon-kumara-rgb-switch-outemu-blue-pt-k552rgb-1-pt-blue-?gclid=Cj0KCQjwqPGUBhDwARIsANNwjV5spiI04VatMISQ7HQd2czGxUMH2T1EsJPk4-_DwnHX0V_ziHJbROMaArp3EALw_wcB"
                     target="_blank"
-                     rel="noopener"
+                    rel="noreferrer"
                   >
                     Teclado Mecânico Gamer - Redragon Kumara, RGB, Switch Outemu
                     Blue
@@ -64,7 +64,7 @@ const Setup = () => {
                   <a
                     href="https://www.kabum.com.br/produto/94555/mouse-gamer-redragon-cobra-chroma-rgb-12400dpi-7-botoes-preto-m711-v2?gclid=Cj0KCQjwqPGUBhDwARIsANNwjV7BnpMSpOnq6T-WvrwTwSPuCLFAAY8-24zUy_tWNdTzfOAP1J_ghfoaAr4FEALw_wcB"
                     target="_blank"
-                     rel="noopener"
+                     rel="noreferrer"
                   >
                     Mouse Gamer - Redragon Cobra, Chroma RGB, 12400DPI
                   </a>
@@ -73,7 +73,7 @@ const Setup = () => {
                   <a
                     href="https://www.kabum.com.br/produto/162665/memoria-gamer-husky-gaming-avalanche-16gb-3200mhz-ddr4-cl19-hgmf008?gclid=Cj0KCQjwqPGUBhDwARIsANNwjV7VivD-ZbdNf4o9Xb2qbc9gSvWzSOKtH2PK6W3tLm_rtUyIjSDCsDwaAjZVEALw_wcB"
                     target="_blank"
-                     rel="noopener"
+                     rel="noreferrer"
                   >
                     Memória Gamer - Husky Gaming, Avalanche,16GB, 3200Mhz, DDR4
                   </a>
@@ -82,7 +82,7 @@ const Setup = () => {
                   <a
                     href="https://www.pichau.com.br/processador-intel-core-i5-10400-6-core-12-threads-2-9ghz-4-3ghz-turbo-cache-12mb-lga1200-bx8070110400?gclid=Cj0KCQjwqPGUBhDwARIsANNwjV6DkzqIEJpBP-EKxm0RL9LShN1HrjudEfOb_l4Lr_3okzEzZlevWGoaAnmoEALw_wcB"
                     target="_blank"
-                     rel="noopener"
+                     rel="noreferrer"
                   >
                     Processador - Intel Core i5-10400
                   </a>
@@ -91,7 +91,7 @@ const Setup = () => {
                   <a
                     href="https://www.kabum.com.br/produto/151567/placa-mae-asus-tuf-gaming-b560m-plus-wifi-intel-lga1200-microatx-ddr4-90mb1770-m0eay0?gclid=Cj0KCQjwqPGUBhDwARIsANNwjV50WtZ0Ai_CcORP87i_e9sfxSjalTIUm9Hst81i-caOe_9knqHJ9CcaAjWKEALw_wcB"
                     target="_blank"
-                     rel="noopener"
+                     rel="noreferrer"
                   >
                     Placa Mãe Asus TUF GAMING B560M-PLUS, Intel LGA1200,
                     microATX, DDR4
@@ -101,7 +101,7 @@ const Setup = () => {
                   <a
                     href="https://www.kabum.com.br/produto/115571/placa-de-video-gigabyte-nvidia-geforce-rtx-2060-d6-6g-gddr6-gv-n2060d6-6gd?gclid=Cj0KCQjwqPGUBhDwARIsANNwjV4RrCl6MUTUOeHSSL0zJbRr5EOd1bprL_hztLb8IGNTl4H_4AH1TKEaAmiFEALw_wcB"
                     target="_blank"
-                     rel="noopener"
+                     rel="noreferrer"
                   >
                     Placa de Vídeo - Gigabyte NVIDIA GeForce RTX 2060 D6 6G,
                     GDDR6
@@ -111,7 +111,7 @@ const Setup = () => {
                   <a
                     href="https://www.kabum.com.br/produto/103281/fonte-xpg-core-reactor-750w-80-plus-gold-modular?gclid=Cj0KCQjwqPGUBhDwARIsANNwjV6y7rDnlZ6XRrvlG7gRs66y4XdzCGjGjC9tBZs9wlOL6dASfcv0XE0aAnd8EALw_wcB"
                     target="_blank"
-                     rel="noopener"
+                    rel="noreferrer"
                   >
                     Fonte - XPG Core Reactor, 750W, 80 Plus Gold Modular
                   </a>
@@ -120,7 +120,7 @@ const Setup = () => {
                   <a
                     href="https://www.mercadolivre.com.br/mouse-pad-gamer-exbom-mp-led3080-300mm-x-800mm-x-4mm-preto/p/MLB15533886?pdp_filters=category:MLB1716#searchVariation=MLB15533886&position=13&search_layout=grid&type=product&tracking_id=2e1b5878-e64e-4239-a4d7-becacd1db688"
                     target="_blank"
-                     rel="noopener"
+                    rel="noreferrer"
                   >
                     Mouse Pad Gamer - Exbom MP-LED3080 300mm x 800mm x 4mm preto
                   </a>
@@ -129,7 +129,7 @@ const Setup = () => {
                   <a
                     href="https://www.kabum.com.br/produto/99866/monitor-lg-23-8-ips-full-hd-hdmi-vesa-ajuste-de-angulo-24mk430h?gclid=Cj0KCQjwqPGUBhDwARIsANNwjV5TJ_LPi4SoXqMaAhDLc6vyQYp-CgYQwWbAhEPMXk7_CBywk5v1NAYaAq4IEALw_wcB"
                     target="_blank"
-                     rel="noopener"
+                   rel="noreferrer"
                   >
                     Monitor - LG 23.8' IPS, Full HD, HDMI, VESA, Ajuste de
                     Ângulo - 24MK430H
@@ -142,17 +142,17 @@ const Setup = () => {
               <h3>Ferramentas</h3>
               <ul>
                 <li>
-                  <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">
+                  <a href="https://code.visualstudio.com/" target="_blank" rel="noreferrer">
                     VS Code
                   </a>
                 </li>
                 <li>
-                  <a href="https://trello.com/pt-BR" target="_blank" rel="noopener">
+                  <a href="https://trello.com/pt-BR" target="_blank" rel="noreferrer">
                     Trello
                   </a>
                 </li>
                 <li>
-                  <a href="https://www.figma.com/" target="_blank" rel="noopener">
+                  <a href="https://www.figma.com/" target="_blank" rel="noreferrer">
                     Figma
                   </a>
                 </li>
@@ -160,13 +160,13 @@ const Setup = () => {
                   <a
                     href="https://www.atlassian.com/br/software/jira"
                     target="_blank"
-                     rel="noopener"
+                    rel="noreferrer"
                   >
                     Jira
                   </a>
                 </li>
                 <li>
-                  <a href="https://git-scm.com/" target="_blank" rel="noopener">
+                  <a href="https://git-scm.com/" target="_blank" rel="noreferrer">
                     Git
                   </a>
                 </li>
@@ -180,13 +180,13 @@ const Setup = () => {
                   <a
                     href="https://marketplace.visualstudio.com/items?itemName=rocketseat.theme-omni"
                     target="_blank"
-                     rel="noopener"
+                    rel="noreferrer"
                   >
                     Tema - Omni Theme
                   </a>
                 </li>
                 <li>
-                  <a href="https://github.com/tonsky/FiraCode" target="_blank" rel="noopener">
+                  <a href="https://github.com/tonsky/FiraCode" target="_blank" rel="noreferrer">
                     Fonte - Fira Code
                   </a>
                 </li>
@@ -200,7 +200,7 @@ const Setup = () => {
                   <a
                     href="https://www.amazon.com.br/gp/product/8532520839/ref=ppx_yo_dt_b_asin_title_o00_s01?ie=UTF8&psc=1"
                     target="_blank"
-                     rel="noopener"
+                    rel="noreferrer"
                   >
                     O design do dia a dia
                   </a>
@@ -209,7 +209,7 @@ const Setup = () => {
                   <a
                     href="https://www.amazon.com.br/gp/product/8576088509/ref=ppx_yo_dt_b_asin_title_o00_s01?ie=UTF8&psc=1"
                     target="_blank"
-                     rel="noopener"
+                   rel="noreferrer"
                   >
                     Não me faça pensar
                   </a>
@@ -218,7 +218,7 @@ const Setup = () => {
                   <a
                     href="https://www.amazon.com.br/gp/product/8576082675/ref=ppx_yo_dt_b_asin_title_o02_s00?ie=UTF8&psc=1"
                     target="_blank"
-                     rel="noopener"
+                    rel="noreferrer"
                   >
                     Código limpo: Habilidades práticas do agile software
                   </a>
@@ -227,7 +227,7 @@ const Setup = () => {
                   <a
                     href="https://www.amazon.com.br/gp/product/8532530907/ref=ppx_yo_dt_b_asin_title_o00_s00?ie=UTF8&psc=1"
                     target="_blank"
-                     rel="noopener"
+                    rel="noreferrer"
                   >
                     Mostre seu trabalho: 10 maneiras de compartilhar sua
                     criatividade e ser descoberto
@@ -237,7 +237,7 @@ const Setup = () => {
                   <a
                     href="https://www.amazon.com.br/gp/product/6555320109/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&psc=1"
                     target="_blank"
-                     rel="noopener"
+                    rel="noreferrer"
                   >
                     Siga em frente: 10 maneiras de manter a criatividade nos
                     bons e maus momentos
@@ -247,7 +247,7 @@ const Setup = () => {
                   <a
                     href="https://www.amazon.com.br/gp/product/8532528422/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&psc=1"
                     target="_blank"
-                     rel="noopener"
+                    rel="noreferrer"
                   >
                     Roube como um artista: 10 dicas sobre criatividade
                   </a>
@@ -256,7 +256,7 @@ const Setup = () => {
                   <a
                     href="https://www.amazon.com.br/gp/product/857416836X/ref=ppx_yo_dt_b_asin_title_o00_s00?ie=UTF8&psc=1"
                     target="_blank"
-                     rel="noopener"
+                    rel="noreferrer"
                   >
                     Design para quem não é designer: Princípios de design e
                     tipografia para iniciantes
